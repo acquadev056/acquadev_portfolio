d476ba1ce6c4d9d10960608ae9a35f528e3fd259
Maximo Gomez Cruz <maximogomezcruz@gmail.com>
2026-08-02 01:55:35 -0600

    chore: set base path to /acquadev_portfolio/ in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://acquadev.github.io',
+    base: '/acquadev_portfolio/',
     output: 'static'
 });
